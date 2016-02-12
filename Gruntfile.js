c14d6d94810cb468094ead88112555aa602aceff
Andy McGuinness <michaelandrewmcguinness@gmail.com>
2016-02-11 21:29:41 -0600

    Adding initial version of delta to repo

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+/*
+ * File: Gruntfile.js
+ * Description: Loads all Grunt plugins and defines tasks
+ * Dependencies: grunt-contrib-sass, grunt-contrib-watch
+ *
+ * @package delta
+ */
+
+module.exports = function(grunt) {
+    /* === Initial Config === */
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    sourcemap: 'none',
+                    style: 'expanded'
+                },
+                files: {
+                    'css/main.css': 'scss/main.scss'
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['scss/*.scss', 'scss/*/*.scss'],
+                tasks: ['sass']
+            }
+        }
+    });
+
+    /* === Loading Plugins === */
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    /* === Register Tasks === */
+    grunt.registerTask('default', ['sass', 'watch']);
+};
