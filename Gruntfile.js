95d2810dc6b3d1274ba99785766ee7ff4e05b26e
Andy McGuinness <michaelandrewmcguinness@gmail.com>
2017-03-05 22:16:13 -0500

    Current status -- adding all pug files and updating everything slash beginning the redesign process

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,16 +23,33 @@ module.exports = function(grunt) {
         },
         watch: {
             scripts: {
-                files: ['src/scss/*.scss', 'src/scss/*/*.scss'],
-                tasks: ['sass']
+                files: ['src/scss/*.scss', 'src/scss/*/*.scss', 'src/*.pug', 'src/*/*.pug'],
+                tasks: ['sass', 'pug']
+            }
+        },
+        pug: {
+            compile: {
+                options: {
+                    data: {
+                        debug: false
+                    }
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['*.pug'],
+                    dest: 'dist/',
+                    ext: '.html'
+                }]
             }
         }
     });
 
     /* === Loading Plugins === */
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-pug');
     grunt.loadNpmTasks('grunt-contrib-watch');
-
+    
     /* === Register Tasks === */
-    grunt.registerTask('default', ['sass', 'watch']);
+    grunt.registerTask('default', ['sass', 'pug:compile', 'watch']);
 };
